baf7e537125e8b4a86894dc26e870b9b7450cd8b
Toom <ta.ka.chi@hotmail.com>
2018-01-17 20:01:31 +0700

    update file2

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="{{ app()->getLocale() }}">
+<html lang="en" class="global xlarge large medium small xsmall">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,12 +7,40 @@
 
         <title>Laravel on Github</title>
         <!--style-->
-
-
-
+        <link href="theme1/css/style.css" rel="stylesheet" type="text/css">
+        <!--<link href="theme1/css/style-lage.css" rel="stylesheet" type="text/css">
+        <link href="theme1/css/style-medium.css" rel="stylesheet" type="text/css">
+        <link href="theme1/css/style-small.css" rel="stylesheet" type="text/css">
+        <link href="theme1/css/style-xlarge.css" rel="stylesheet" type="text/css">
+        <link href="theme1/css/style-xsmall.css" rel="stylesheet" type="text/css">-->
+        <link href="theme1/css/lity.css" rel="stylesheet" type="text/css">
         <!--/style-->
+
+        <!-- script -->
+        <!--<script src="theme1/js/jssor.slider-21.1.6.min.js" type="text/javascript"></script>
+        <script src="theme1/js/lity.js" type="text/javascript"></script>
+        
+        <script src="theme1/js/skel.min.js" type="text/javascript"></script>-->
+        <script src="theme1/js/skel-layers.min.js" type="text/javascript"></script>
+        <!-- /script -->
     </head>
-<body>
-xxx
+<body id="top">
+<div id="skel-layers-Wrapper" style="position: relative; left: 0px; right: 0px; top: 0px; backface-visibility: hidden; transition: -webkit-transform 0.25s ease-in-out; z-index: auto; transform: translate(0px, 0px);">
+    
+<div id="skel-layers-visibleWrapper" style="position: relative;">        
+    <!-- Header -->
+    <header id="header" class="skel-layers-fixed">
+	    <h1><a href="#"><img src='theme1/images/logo.png' height=73 TITLE='TopDollarDating'></a></h1>
+        <div id="skel-layers-placeholder-nav"></div>
+    </header>
+    <!-- End Header -->
+            <div id="navButton" class="skel-layer skel-layer-top-left" style="backface-visibility: hidden; transition: -webkit-transform 0.25s ease-in-out; z-index: 10004; position: fixed; display: block; width: 6em; height: 4em; top: 0px; left: 0px; transform: translate(0px, 0px);">
+            <span class="toggle" data-action="toggleLayer" data-args="navPanel" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); cursor: pointer;">
+            
+            </span>
+            </div>
+    
+</div>        
+</div>
 </body>
 </html>
